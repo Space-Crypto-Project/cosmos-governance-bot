28feaa336475f4f164d51bfe8b07292fc39b610a
dutchybeatz <dutchycorp@pm.me>
2024-08-19 15:30:35 +0200

    babylon removed

--- ChainApis.py
+++ ChainApis.py
@@ -360,14 +360,6 @@ chainAPIs = {
         },
         "@Pryzm_Zone"
     ],
-    "babylon_testnet": [
-        'https://babylon-testnet-api.spacestake.tech/cosmos/gov/v1beta1/proposals',
-        {
-            "ping": 'https://explorer.spacestake.tech/babylon-testnet/gov',
-
-        },
-        "@babylon_chain"
-    ],
     "lava_testnet": [
         'https://lava-testnet-api.spacestake.tech/cosmos/gov/v1beta1/proposals',
         {
